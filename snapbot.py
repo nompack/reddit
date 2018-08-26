2a19136c3c75cb5daf0335a9421bf8072d6d5dc5
SuhasHariharan <36813010+SuhasHariharan@users.noreply.github.com>
2018-08-26 12:45:44 +0800

    commited!

---
+++ snapbot.py
@@ -0,0 +1,47 @@
+
+ 
+import praw
+import time
+
+import requests
+import requests.auth
+
+reddit = praw.Reddit(client_id='1rRYW4ILxwgADQ',
+                     client_secret='heJu5lZGOClfEWnNR8CC3NFPZw4',
+                     username='hacksorskillbot',
+                     password='fastlol1234',
+                     user_agent='bot by hacksorskill')
+                    
+
+client_auth = requests.auth.HTTPBasicAuth('1rRYW4ILxwgADQ', 'heJu5lZGOClfEWnNR8CC3NFPZw4')
+post_data = {"grant_type": "password", "username": "hacksorskillbot", "password": "fastlol1234"}
+headers = {"User-Agent": "personalbot/0.1 by hacksorskill"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+resp = response.json()
+headers = {"Authorization": "bearer " + resp['access_token'], "User-Agent": "personalbot/0.1 by hacksorskill"}
+response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+resp=response.json()
+
+
+        
+keyphrase = 'snap'
+
+
+# look for phrase and reply appropriately
+subreddit = reddit.subreddit('thanosdidnothingwrong')
+alreadydone=[]
+
+for submission in subreddit.stream.comments():
+    if keyphrase in submission.body.lower() and submission.id not in alreadydone:
+        print (submission.body)
+        try:
+            submission.reply('You said SNAP! The hardest decisions require the strongest wills')
+            alreadydone.append(submission.id)
+        except:
+            print("You have been rate limited trying again in 3 minutes")
+            time.sleep(180)
+            continue
+        
+
+            
+        
