58f466ad587895e1260c8e70cbae65309bb3f37d
nompack <42458172+nompack@users.noreply.github.com>
2018-09-20 15:44:46 +0800

    Update redditbot.py

--- redditbot.py
+++ redditbot.py
@@ -1,6 +1,51 @@
-from flask import Flask, request
-app = Flask(__name__)
-@app.route('/', methods=['POST'])
-def result():
-    print(request.form) # should display 'bar'
-    return 'Received !' # response to your request.
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        self._set_headers()
+        post_data=str(post_data)
+        post_data=post_data[2:len(post_data)-1]
+        print(post_data)
+        
+        
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print ('Starting httpd...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
