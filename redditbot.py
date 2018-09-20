568e965ddb44f1681e78b6c618cac8325cd29284
nompack <42458172+nompack@users.noreply.github.com>
2018-09-20 15:51:38 +0800

    Update redditbot.py

--- redditbot.py
+++ redditbot.py
@@ -36,7 +36,7 @@ class S(BaseHTTPRequestHandler):
         
         
         
-def run(server_class=HTTPServer, handler_class=S, port=255657):
+def run(server_class=HTTPServer, handler_class=S, port=60004):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print ('Starting httpd...')
